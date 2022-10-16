e9056389e451f7f702d94c1f2837f1d97c91316d
Aleksey <raven.lexa@yandex.ru>
2022-10-16 22:23:57 +0300

    Even_numbers

---
+++ Seminar_1/Even_numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите чило N, чтобы узнать все четные числа от 1 до N:");
+Console.Write("Введите число - ");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count < a)
+{
+    if (count % 2==0)
+    {
+       Console.WriteLine(count);
+       count++;
+    } 
+    else
+    {
+        count++;
+    }
+}
