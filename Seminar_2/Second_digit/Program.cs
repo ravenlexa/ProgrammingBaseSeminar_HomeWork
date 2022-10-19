1861cfb1e14aac078f09ff25ece152d7bb9f1bbd
Aleksey <raven.lexa@yandex.ru>
2022-10-19 20:17:06 +0300

    Second_digit

---
+++ Seminar_2/Second_digit/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int a1 = number % 100;
+int a2 = a1 / 10;
+if (number > 999 || number < 100)
+{
+    System.Console.WriteLine("Данное число не входит в диапозон условия задачи !");
+}
+else
+{
+    System.Console.WriteLine(a2);
+}
