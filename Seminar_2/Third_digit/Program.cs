96844a2ecb2f803dff9de7439aa87a8d910b9ed0
Aleksey <raven.lexa@yandex.ru>
2022-10-19 22:37:40 +0300

    Third_digit

---
+++ Seminar_2/Third_digit/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число и я укажу третью цифру слева");
+int number = Convert.ToInt32(Console.ReadLine());
+int a1 = 10;
+if (number < 100)
+{
+    System.Console.WriteLine("Нет третьей цифры");
+}
+else
+{    while (number > 1000)
+    {
+        number = number / a1;
+    }
+    int a2 = number % 10;
+    System.Console.WriteLine(a2);
+}
+//if (number < 100) System.Console.WriteLine("Нет");
