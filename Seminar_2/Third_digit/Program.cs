35f054d8406baa686d80bac3922afb950cc88cc7
Aleksey <raven.lexa@yandex.ru>
2022-10-20 19:35:17 +0300

    Third_digit

--- Seminar_2/Third_digit/Program.cs
+++ Seminar_2/Third_digit/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите число и я укажу третью цифру слева");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число и я укажу третью цифру слева");
 int number = Convert.ToInt32(Console.ReadLine());
 int a1 = 10;
 if (number < 100)
@@ -13,4 +15,4 @@ else
     int a2 = number % 10;
     System.Console.WriteLine(a2);
 }
-//if (number < 100) System.Console.WriteLine("Нет");
+
