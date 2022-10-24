0873242f5c7d7a6c171d6659794068f56a319d5d
Aleksey <raven.lexa@yandex.ru>
2022-10-24 21:22:38 +0300

    Cube_Range

---
+++ Seminar_3/Cube_Range/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. */
+// Выдает таблицу кубов чисел от 1 до N.
+void PrintCubsToConsole(int n)
+{
+    if (n >= 1)
+    {    
+        for (int i = 1; i <= n; i++)
+        {
+            System.Console.WriteLine(i * i * i);
+        }
+    }
+    else
+    {
+        System.Console.WriteLine("Введи число согласно условию задачи");
+    }    
+}
+System.Console.Write( "Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintCubsToConsole(number);
