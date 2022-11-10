e68a1eb69396786dcd835baef9c16af4716cf3a9
Aleksey <raven.lexa@yandex.ru>
2022-11-10 20:06:14 +0300

    Sum_of_digits

---
+++ Seminar_4/Sum_of_digits/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе. Для решения задания использование цикла for является обязательным условием*/
+int SumofDigits(int number)
+{
+    string strNumber = Math.Abs(number).ToString();
+    int sum = 0;
+    for (int i = 0; i < strNumber.Length; i++)
+    {
+        sum = sum + int.Parse(Convert.ToString(strNumber[i])); //Вот это подсмотрел. Писал просто sum = sum + strNumber[i] и ничего не получалось.
+    }
+   
+    return sum;
+}
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = SumofDigits(number);
+Console.WriteLine(sum);
