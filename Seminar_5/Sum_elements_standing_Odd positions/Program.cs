ac79d75949819fd80d77bd84c60a84431bcd77b5
Aleksey <raven.lexa@yandex.ru>
2022-11-12 18:10:38 +0300

    Even_numbers_in_the_Array

---
+++ Seminar_5/Sum_elements_standing_Odd positions/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.*/
+
+int SumOddPsition(int[] array)
+{
+    int SumOdd = 0; 
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            SumOdd = SumOdd + array[i];
+             i++;
+        }
+    }
+    return SumOdd;
+}
+
+
+int[] array = new int [new Random().Next(5, 11)];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(10, 99);
+    System.Console.Write(array[i] + " ");
+}
+int sum = SumOddPsition(array); 
+System.Console.WriteLine(sum);
