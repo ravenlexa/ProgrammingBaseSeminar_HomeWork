4d94275ea9d99ffd76ccaa053164dab02010b1c6
Aleksey <raven.lexa@yandex.ru>
2022-11-12 12:15:00 +0300

    Array_Numbers

---
+++ Seminar_4/Array_Numbers/Program.cs
@@ -0,0 +1,7 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. */
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 99);
+    System.Console.Write(array[i] + " ");
+}
