5c14990b4a3ef045b733dce2ddf09db78a7b10c5
Aleksey <raven.lexa@yandex.ru>
2022-11-12 20:48:45 +0300

    Max_Min_Difference

---
+++ Seminar_5/Max_Min_Difference/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.*/
+
+double DifferenceMaxMin(double[] array)
+{
+    double min = array[0];
+    double max = array[0];
+    double diff = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= max)
+        {
+            max = array[i];
+        }
+        else if (array[i] <= min)
+        {
+            min = array[i];
+        }
+    }
+    System.Console.WriteLine("Максимальное число = " + max);
+    System.Console.WriteLine("Минимальное число = " + min);
+    diff = max - min;
+    return diff;
+}
+
+double[] array = new double[7] {5.44, 4.8, 45.5, 87.4, 56.5, 101.5, 45.6};
+System.Console.WriteLine("Разница между максимальным и минимальным чилом = " + DifferenceMaxMin(array));
+
+
