7884397dd2594bdc1fffc323018ac36d62355df3
Aleksey <raven.lexa@yandex.ru>
2022-11-15 18:16:33 +0300

    Numbers_greater_Zero

--- Seminar_6/Numbers_greater_Zero/Program.cs
+++ Seminar_6/Numbers_greater_Zero/Program.cs
@@ -1,7 +1,7 @@
 ﻿/* Пользователь вводит с клавиатуры M чисел. 
 Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
 Console.Write("Введите элементы(через пробел): ");
-int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse); //Подсмотрел как сделать не фиксированное количество чисел, которое может ввести пользователь.
 int count = 0;
  
 for (int i = 0; i < array.Length; i++)
