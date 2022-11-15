6bcf1c0ed4caded290a914766b60166aee301f64
Aleksey <raven.lexa@yandex.ru>
2022-11-15 18:14:54 +0300

    Numbers_greater_Zero

---
+++ Seminar_6/Numbers_greater_Zero/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
+Console.Write("Введите элементы(через пробел): ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0: {count}");
