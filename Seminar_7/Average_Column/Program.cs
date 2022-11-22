2f418f41e4825d72f69c46574f931b6155b03421
Aleksey <raven.lexa@yandex.ru>
2022-11-22 22:05:58 +0300

    Average_Column

---
+++ Seminar_7/Average_Column/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Запросите от пользователя размерности двумерного массива. 
+Напишите метод для заполнения массива случайными числами. 
+Напишите метод для поиска ср. арифметического значения каждого столбца.*/
+int[,] FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+double AverageColumn(int[,] array)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            sum = sum + array[j, i];
+        }
+        Console.WriteLine(Math.Round(sum / array.GetLength(1), 2));
+        sum = 0;
+    }
+    return sum;    
+}
+
+Console.WriteLine("Введите число строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+Console.WriteLine();
+FillArray(array);
+Console.WriteLine();
+AverageColumn(array);
+
