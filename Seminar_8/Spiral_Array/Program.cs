822bb06501cc1eb52abb2a9c034d7df919da68eb
Aleksey <raven.lexa@yandex.ru>
2022-11-27 21:14:35 +0300

    Spiral_Array

---
+++ Seminar_8/Spiral_Array/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Напишите программу, которая заполнит спирально массив 4 на 4.*/
+void FillArray(int[,] array)
+{
+    int number = 1;
+    int i = 0;
+    int j = 0;
+
+    while (number <= array.GetLength(0) * array.GetLength(1))
+    {
+        array[i, j] = number;
+        number++;
+        if (i <= j + 1 && i + j < array.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= array.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > array.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("{0,5}", array[i, j]);
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
+
+int n = 4;
+int[,] snailArray = new int[n, n];
+
+FillArray(snailArray);
+PrintArray(snailArray);
