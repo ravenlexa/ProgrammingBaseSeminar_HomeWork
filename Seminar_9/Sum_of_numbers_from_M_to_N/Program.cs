519ebce08c9de32401bd694fd7f2bffa8c09f751
Aleksey <raven.lexa@yandex.ru>
2022-11-28 19:21:24 +0300

    Sum_of_numbers_from_M_to_N

---
+++ Seminar_9/Sum_of_numbers_from_M_to_N/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+void SumRangeNumbers(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+int SumMN(int m, int n)
+{
+    int result = m;
+    if (m == n)
+    {
+        return 0;
+    }
+    else
+    {
+        m++;
+        result = m + SumMN(m, n);
+        return result;
+    }
+}
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+SumRangeNumbers(m, n);
