4a1fdaeeb3a364834907adc4f5e66444fde68cfb
Aleksey <raven.lexa@yandex.ru>
2022-11-28 19:12:03 +0300

    Numbers_from_N_to_1

---
+++ Seminar_9/Numbers_from_N_to_1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задайте значения M и N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от M до N.*/
+
+string NumbersRange(int M, int N)
+{
+    if (M <= N) 
+    {
+        return NumbersRange(M, N -1) + $"{N}, ";
+    }
+    else return String.Empty;
+}
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(NumbersRange(M, N));
